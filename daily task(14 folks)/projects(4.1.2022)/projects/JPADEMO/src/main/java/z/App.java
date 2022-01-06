66563f8108ca429779ce072ed0c238a2d099ebf8
mercy joice <91773567+Mercyjoice@users.noreply.github.com>
2022-01-06 11:39:31 +0530

    Add files via upload

---
+++ daily task(14 folks)/projects(4.1.2022)/projects/JPADEMO/src/main/java/z/App.java
@@ -0,0 +1,35 @@
+package z;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("hi");
+		Session s = Helper.getSession();
+		System.out.println(s);
+//		Employee e = new Employee(100, "amit", "kolkata");
+//		Transaction tr = s.beginTransaction();
+//		s.save(e);
+//		tr.commit();
+//		s.close();
+		System.out.println("employee saved!!! Check MYSQL!!!!");
+		System.out.println("--------------------fetch employee---------------");
+		Employee e1 = (Employee) s.load(Employee.class, 100);
+		System.out.println(e1.getEname() + "   " + e1.getAddress());
+		System.out.println("--------------------Update employee---------------");
+		e1.setAddress("Delhi");
+		Transaction tr = s.beginTransaction();
+		s.update(e1);
+		tr.commit();
+		s.close();
+		System.out.println("data updated check mysql!!!!");
+		// s.delete(e1);
+
+	}
+}
---
+++ daily task(14 folks)/projects(4.1.2022)/projects/JPADEMO/src/main/java/z/Employee.java
@@ -0,0 +1,27 @@
+package z;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "capgemini")
+class Employee {
+
+	@Id
+	@Column(name = "testid")
+	private int id;
+	@Column(name = "testename")
+	private String ename;
+	@Column(name = "testaddress")
+	private String address;
+
+}
